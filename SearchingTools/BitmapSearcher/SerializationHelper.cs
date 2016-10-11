f2135d67685a686b2255f458b412b3d03a653c87
UnresolvedExternal <vmi1401bsv04@gmail.com>
2016-10-11 06:36:18 +0600

    Renamed folder WrappedSearcher to BitmapSearcher

---
+++ SearchingTools/BitmapSearcher/SerializationHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace SearchingTools
+{
+	/// <summary>
+	/// Сериализует объекты BitmapSearcher
+	/// </summary>
+	internal static class SerializationHelper
+	{
+		private static DataContractJsonSerializer formatter =
+			new DataContractJsonSerializer(typeof(BitmapSearcher));
+
+		public static void Serialize(BitmapSearcher obj, Stream output)
+		{
+			using (var memory = new MemoryStream())
+			{
+				formatter.WriteObject(memory, obj);
+				memory.WriteTo(output);
+			}
+		}
+
+		/// <summary>
+		/// Десериализует объект. Восстанавливает поток в случае ошибки.
+		/// </summary>
+		/// <param name="input"></param>
+		/// <returns></returns>
+		public static BitmapSearcher Deserialize(Stream input)
+		{
+			var oldPosition = input.Position;
+			BitmapSearcher result;
+			try
+			{
+				result = (BitmapSearcher)formatter.ReadObject(input);
+			}
+			catch
+			{
+				input.Position = oldPosition;
+				throw;
+			}
+			return result;
+		}
+	}
+}
